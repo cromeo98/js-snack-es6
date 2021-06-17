c67f3a896eaab6dd00828117a83ea6fe95168194
cromeo98 <romeo.christian98@gmail.com>
2021-06-17 08:04:10 +0200

    solution 1

--- js-snack-2/js/script.js
+++ js-snack-2/js/script.js
@@ -22,3 +22,27 @@ const teams = [
     }
 ];
 
+const generateRandomPts = () => Math.floor(Math.random() * (50 - 0 + 1) ) + 0;
+const generateRandomFouls = () => Math.floor(Math.random() * (80 - 20 + 1) ) + 20;
+
+// sostituzione n. random 1
+for(let i = 0; i < teams.length; i++){
+    teams[i].punti = generateRandomPts();
+    teams[i].falli = generateRandomFouls();
+}
+
+// sostituzione n. random 2
+// for(let i = 0; i < teams.length; i++){
+//     for(let k in teams[i]){
+//         if(k == 'punti'){
+//             teams[i][k] = generateRandomPts();
+//         } else if(k == 'falli'){
+//             teams[i][k] = generateRandomFouls();
+//         }
+//     }
+// }
+
+// soluzione 1
+for (const{nome: n, falli: f} of teams){
+    console.log(n + f);
+}
